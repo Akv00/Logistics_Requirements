05f7667f2168c9370a3260b0e2d7d6355d458bea
Akv00 <kunal613.kv@gmail.com>
2024-06-13 05:06:09 +0530

    changed requirements to array of string

--- app/crud.py
+++ app/crud.py
@@ -5,16 +5,17 @@ def get_configuration(db: Session, country_code: str):
     return db.query(models.Configuration).filter(models.Configuration.country_code == country_code).first()
 
 def create_configuration(db: Session, configuration: schemas.ConfigurationCreate):
-    db_configuration = models.Configuration(**configuration.dict())
+    db_configuration = models.Configuration(country_code=configuration.country_code,
+        requirements=configuration.requirements)
     db.add(db_configuration)
     db.commit()
     db.refresh(db_configuration)
     return db_configuration
 
-def update_configuration(db: Session, country_code: str, requirements: schemas.ConfigurationUpdate):
+def update_configuration(db: Session, country_code: str, configuration: schemas.ConfigurationUpdate):
     db_configuration = get_configuration(db, country_code)
     if db_configuration:
-        db_configuration.requirements = requirements.dict()["requirements"]
+        db_configuration.requirements = configuration.requirements
         db.commit()
         db.refresh(db_configuration)
     return db_configuration
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel, Field
-from typing import Dict, Any
+from typing import Any, List
 
 class ConfigurationBase(BaseModel):
     country_code: str
-    requirements: Dict[str, Any]
+    requirements: List[str]
 
 class ConfigurationCreate(ConfigurationBase):
     country_code: str = Field(..., min_length=2, max_length=3, description="Country code (ISO 3166-1 alpha-2)")
 
 class ConfigurationUpdate(BaseModel):
-    requirements: Dict[str, Any]
+    requirements: List[str]
 
 class Configuration(ConfigurationBase):
     id: int
