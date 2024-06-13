b005203af5be1c814cab228efbd985bf1169aa9e
Akv00 <kunal613.kv@gmail.com>
2024-06-13 05:57:50 +0530

    logging functionality added

--- app/exceptions.py
+++ app/exceptions.py
@@ -7,6 +7,12 @@ class ConfigurationNotFoundError(HTTPException):
         super().__init__(status_code=404, detail=detail)
 
 
+class ConfigurationAlreadyExists(HTTPException):
+    def __init__(self, country_code: str):
+        detail = f"Configuration for country code {country_code} already exists"
+        super().__init__(status_code=404, detail=detail)
+
+
 def add_exception_handlers(app):
     @app.exception_handler(HTTPException)
     async def http_exception_handler(request: Request, exc: HTTPException):
--- app/routes/config.py
+++ app/routes/config.py
@@ -1,19 +1,25 @@
+import logging
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .. import schemas, crud, database
-from ..exceptions import ConfigurationNotFoundError
+from ..exceptions import ConfigurationAlreadyExists, ConfigurationNotFoundError
+
+
 
 router = APIRouter()
+
+logger = logging.getLogger(__name__)
 
 @router.post("/create_configuration", response_model=schemas.Configuration)
 def create_configuration(configuration: schemas.ConfigurationCreate, db: Session = Depends(database.get_db)):
     try:
         db_configuration = crud.get_configuration(db, configuration.country_code)
         if db_configuration:
-            raise HTTPException(status_code=400, detail="Configuration already exists")
+            raise ConfigurationAlreadyExists(country_code=configuration.country_code)
         return crud.create_configuration(db, configuration)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logger.error(f"Error creating configuration: {e}", exc_info=True)
+        raise HTTPException(status_code=500,  detail=str(e))
     # db_configuration = crud.get_configuration(db, configuration.country_code)
     # if db_configuration:
     #     raise HTTPException(status_code=400, detail="Configuration already exists")
@@ -46,7 +52,7 @@ def delete_configuration(country_code: str, db: Session = Depends(database.get_d
     try:
         db_configuration = crud.get_configuration(db, country_code)
         if not db_configuration:
-            raise HTTPException(status_code=404, detail="Configuration not found")
+            raise ConfigurationNotFoundError(country_code)
         return crud.delete_configuration(db, country_code)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
